5dd36e2b710bfe12409da98c92a46987a9657719
ssobelek <45128863+ssobelek@users.noreply.github.com>
2022-05-24 21:50:14 +0300

    Yeni hali

--- Kurban Yönetim/Database.py
+++ Kurban Yönetim/Database.py
@@ -32,15 +32,64 @@ def kurban_no_guncelle(eski,yeni):
 def isim_guncelle(eski,yeni):
     cursor.execute("UPDATE kurban set Hissedarlar = ? where Hissedarlar = ? ",(yeni,eski))
     con.commit()
-# tablo()
-# Hissedarlar = input("İsminiz: ")
-# Kurbankilo = int(input("Kurbanın kilosu: "))
-# Hissesayisi = int(input("Hisse sayısı: "))
-# Kurbanno = int(input("Kurban numarası: "))
-# kurbanekle(Hissedarlar,Kurbankilo,Hissesayisi,Kurbanno)
-# hissedarekle()
-# hissedarsil()
-# kurban_no_guncelle(3,5)
-# isim_guncelle()
-verilerial()
-con.close()
+
+print(
+"""
+******************************************************************************************************
+                    Kurban Yönetim Sistemine Hoşgeldiniz...  
+                                          
+                                :YAPABİLECEĞİNİZ İŞLEMLER:                                          
+
+* q == Programı kapatmak için kullanınız.
+
+*kurbanekle = Hisse sahipleri, Kurbanın kilosu, Pay sayısı, Kurban numarasını içerir.
+
+*hissedarsil = Adını ve soyadını yazdıgınız hissedarın tüm bilgilerini siler.
+
+*kurbanno = Önce eski sonra yeni vermek istediğiniz kurban numarasını yazın ve değiştirmiş olun.
+
+*isimdeğiş = Önce değiştirmek istediğiniz sonra koymak istediğiniz ismi yazın ve değiştirin.
+
+*bilgilerigör = Bütün bilgileri gösterir.
+******************************************************************************************************
+""")
+while True:
+    s = input("İşlemi yazınız: ")
+
+    if  s == "q":
+        break
+
+    elif s == "hissedarsil":
+        hissedar = input("Silinecek isimi tam yazınız: ")
+        hissedarsil(hissedar)
+        print("{} kullanıcısı silindi.".format(hissedar))
+        continue
+
+    elif s == "kurbanekle":
+        Hissedarlar = input("İsminiz: ")
+        Kurbankilo = int(input("Kurbanın kilosu: "))
+        Hissesayisi = int(input("Hisse sayısı: "))
+        Kurbanno = int(input("Kurban numarası: "))
+        kurbanekle(Hissedarlar,Kurbankilo,Hissesayisi,Kurbanno)
+        print(verilerial())
+        continue
+
+    elif s == "isimdeğiş":
+        eski = input("Değiştirmek istediğiniz ismi giriniz: ")
+        yeni = input("Yeni ismi giriniz: ")
+        isim_guncelle(eski,yeni)
+        print("'{}' ismi '{}' olarak güncellendi".format(eski,yeni))
+
+    elif s == "kurbanno":
+        eski = input("Değiştirmek istediğiniz kurbanın numarasını giriniz: ")
+        yeni = input("Yeni numarayı giriniz: ")
+        kurban_no_guncelle(eski,yeni)
+        print("'{}' ismi '{}' olarak güncellendi".format(eski, yeni))
+        continue
+    elif s == "bilgilerigör":
+        print(verilerial())
+        continue
+    else:
+        print("Lütfen komutu doğru girdiğinize emin olun.")
+        continue
+    con.close()
